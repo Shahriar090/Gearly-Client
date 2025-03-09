64f9f7c9914d600887cc92c0c49841408b823d24
Shahriar <shahriarh655@gmail.com>
2025-03-10 04:51:14 +0600

    Fixing user profile cors issue

--- src/pages/dashboard/user/UserProfile.tsx
+++ src/pages/dashboard/user/UserProfile.tsx
@@ -1,22 +1,28 @@
 import useAxios from "@/hooks/useAxios";
 import { useEffect, useState } from "react";
 import { TUserProfile } from "./user.types";
+import { useAuth } from "@/hooks/useAuth";
 
 const UserProfile = () => {
   const [user, setUser] = useState<TUserProfile | null>(null);
   const [loading, setLoading] = useState(false);
 
   const { api } = useAxios();
+  const { auth } = useAuth();
 
   useEffect(() => {
     const fetchProfile = async () => {
       try {
         setLoading(true);
+        const token = auth.accessToken;
         const response = await api.get(
-          `${import.meta.env.VITE_SERVER_BASE_URL}/users/profile`
+          `${import.meta.env.VITE_SERVER_BASE_URL}/users/profile`,
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
         );
-
-        console.log(response.data?.data?.name?.firstName, "From profile");
 
         setUser(response.data.data);
       } catch (error) {
@@ -26,7 +32,7 @@ const UserProfile = () => {
       }
     };
     fetchProfile();
-  }, [api]);
+  }, [api, auth.accessToken]);
 
   if (loading) return <p>Fetching Profile..</p>;
   return (
