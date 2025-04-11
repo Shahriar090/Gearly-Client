1eb6dfe808eed314df0fe1497fb01d7ae2886918
Shahriar <shahriarh655@gmail.com>
2025-04-11 20:13:37 +0600

    Updated flash sale component in the home page. Replaced the static flash sale data with dynamic server data.

--- src/pages/home/flash-sale/FlashSale.tsx
+++ src/pages/home/flash-sale/FlashSale.tsx
@@ -1,11 +1,37 @@
-import { flashSaleProducts } from "./flash-sale-data";
 import { Card } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Eye, Heart } from "lucide-react";
 import Timer from "./Timer";
 import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+import { TFlashSaleItem } from "@/pages/dashboard/admin/flash-sales/flashSales.types";
+import useAxios from "@/hooks/useAxios";
 
 const FlashSale = () => {
+  const [flashSales, setFlashSales] = useState<TFlashSaleItem[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const { api } = useAxios();
+
+  // fetch flash sales
+  useEffect(() => {
+    const fetchFlashSales = async () => {
+      try {
+        setLoading(true);
+        const response = await api.get<{ data: TFlashSaleItem[] }>(
+          `${import.meta.env.VITE_LOCAL_SERVER_URL}/flash-sales`
+        );
+
+        setFlashSales(response.data?.data || []);
+      } catch (error) {
+        setError("Failed To Fetch Flash Sales");
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchFlashSales();
+  }, [api]);
   return (
     <div className="main-container mt-16 md:mt-6 px-4">
       <div className="bg-white">
@@ -39,40 +65,53 @@ const FlashSale = () => {
         <div className="w-full h-0.5 bg-gray-100 mt-2"></div>
 
         {/* products */}
-        <div className="products grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-3 mt-4 p-4">
-          {flashSaleProducts.map((product) => (
-            <Card
-              key={product.id}
-              className="relative p-4 cursor-pointer shadow-none"
-            >
-              <Badge className="absolute top-2 left-2 bg-[var(--color-blue)] text-[var(--color-text)]">
-                -{product.discount}%
-              </Badge>
-              <div className="absolute right-2 top-2 space-y-1">
-                <div>
+        {loading ? (
+          <div className="p-4">Loading Flash Sales...</div>
+        ) : error ? (
+          <div className="p-4 text-red-500">{error}</div>
+        ) : flashSales.length === 0 ? (
+          <div className="p-4">No Flash Sales Available</div>
+        ) : (
+          <div className="products grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-3 mt-4 p-4">
+            {flashSales.map((item) => (
+              <Card
+                key={item._id}
+                className="relative p-4 cursor-pointer shadow-none md:border-none md:hover:shadow-md transition-all duration-500"
+              >
+                <Badge className="absolute top-2 left-2 bg-[var(--color-blue)] text-[var(--color-text)]">
+                  -{item.discount}%
+                </Badge>
+                <div className="absolute right-2 top-2 space-y-1">
                   <Heart className="h-9 w-9 bg-[var(--color-text)] p-2 rounded-full" />
-                </div>
-                <div>
                   <Eye className="h-9 w-9 bg-[var(--color-text)] p-2 rounded-full" />
                 </div>
-              </div>
-              <img
-                src={product.image}
-                alt={product.name}
-                className="w-32 h-32 mx-auto  mb-3"
-              />
-              <div className="flex flex-col items-start gap-1">
-                <h3 className="text-sm font-medium">{product.name}</h3>
-                <span className="text-sm font-medium text-[var(--color-blue)]">
-                  ${product.price - (product.price * product.discount) / 100}
-                </span>
-                <span className="text-sm line-through text-[var(--color-red)]">
-                  ${product.price}
-                </span>
-              </div>
-            </Card>
-          ))}
-        </div>
+
+                <img
+                  src={item.product.images?.[0]}
+                  alt={item.product.modelName}
+                  className="w-full h-32 mx-auto"
+                />
+
+                <div className="flex flex-col items-start gap-1">
+                  <h3 className="text-sm font-medium line-clamp-1 text-[var(--color-black)]">
+                    {item.product.modelName}
+                  </h3>
+                  <span className="text-sm font-medium text-[var(--color-blue)]">
+                    $
+                    {(
+                      item.product.price -
+                      (item.product.price * item.discount) / 100
+                    ).toFixed(2)}
+                  </span>
+
+                  <span className="text-sm line-through text-[var(--color-red)]">
+                    ${item.product.price}
+                  </span>
+                </div>
+              </Card>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
