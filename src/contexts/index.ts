6250d508617dc12cbc2eb42dd3c0a0093380a01e
Shahriar Hossain <shahriarh655@gmail.com>
2026-05-03 00:01:28 +0600

    feat(attribute-template): implemented context for attribute template

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,4 +1,5 @@
 import { TCartContext } from "@/pages/dashboard/user/cart/cart.types";
+import { AttributeTemplateContextType } from "@/types/attribute_template/attribute_template_context_type";
 import { TAuthContext } from "@/types/authTypes";
 import { createContext } from "react";
 
@@ -6,4 +7,7 @@ const AuthContext = createContext<TAuthContext | undefined>(undefined);
 
 const CartContext = createContext<TCartContext | undefined>(undefined);
 
-export { AuthContext, CartContext };
+const AttributeTemplateContext =
+  createContext<AttributeTemplateContextType | null>(null);
+
+export { AttributeTemplateContext, AuthContext, CartContext };
