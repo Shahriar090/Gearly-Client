a5b7ca534c90120b8b1ddf39a4b8ae7579e1e013
Shahriar Hossain <shahriarh655@gmail.com>
2026-05-04 23:46:09 +0600

    feat(attribute-template): implemented AddGroupButton component for AttributeTemplate

---
+++ src/pages/dashboard/admin/attribute_template/components/AddGroupButton.tsx
@@ -0,0 +1,21 @@
+import { useAttributeTemplate } from "@/hooks/useAttributeTemplate";
+
+const AddGroupButton = () => {
+  const { addGroup } = useAttributeTemplate();
+
+  const handleAdd = () => {
+    const name = prompt("Enter group name");
+
+    if (!name) return;
+
+    addGroup(name);
+  };
+  return (
+    <button
+      onClick={handleAdd}
+      className="border border-dashed p-4 w-full text-center"
+    ></button>
+  );
+};
+
+export default AddGroupButton;
