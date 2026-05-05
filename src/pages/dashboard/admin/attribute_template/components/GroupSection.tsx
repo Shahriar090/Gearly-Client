634e77df4250a0a9aa4f42da7c51fcd672b851fa
Shahriar Hossain <shahriarh655@gmail.com>
2026-05-06 00:59:52 +0600

    feat(attribute-template): updated GroupSection

--- src/pages/dashboard/admin/attribute_template/components/GroupSection.tsx
+++ src/pages/dashboard/admin/attribute_template/components/GroupSection.tsx
@@ -2,11 +2,19 @@ import { GroupState } from "@/types/attribute_template/attribute_template_types"
 import AddGroupButton from "./AddGroupButton";
 import GroupCard from "./GroupCard";
 
-const GroupSection = ({ groups }: { groups: GroupState[] }) => {
+type GroupSectionProps = {
+  groups: GroupState[];
+  onAddAttribute: (groupId: string) => void;
+};
+const GroupSection = ({ groups, onAddAttribute }: GroupSectionProps) => {
   return (
     <div className="space-y-4">
       {groups.map((group) => (
-        <GroupCard key={group.id} group={group} />
+        <GroupCard
+          key={group.id}
+          group={group}
+          onAddAttribute={onAddAttribute}
+        />
       ))}
       <AddGroupButton />
     </div>
