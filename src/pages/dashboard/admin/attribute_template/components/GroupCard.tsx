69224bf2898d633e7fa07eb488e2b7d464d931bf
Shahriar Hossain <shahriarh655@gmail.com>
2026-05-06 00:56:51 +0600

    feat(attribute-template): updated GroupCard component. Added Edit and Delete Group feature

--- src/pages/dashboard/admin/attribute_template/components/GroupCard.tsx
+++ src/pages/dashboard/admin/attribute_template/components/GroupCard.tsx
@@ -1,18 +1,74 @@
+import { useAttributeTemplate } from "@/hooks/useAttributeTemplate";
 import { GroupState } from "@/types/attribute_template/attribute_template_types";
+import { useState } from "react";
 
-const GroupCard = ({ group }: { group: GroupState }) => {
+type GroupCardProps = {
+  group: GroupState;
+  onAddAttribute: (groupId: string) => void;
+};
+
+const GroupCard = ({ group, onAddAttribute }: GroupCardProps) => {
+  const { updateGroup, deleteGroup } = useAttributeTemplate();
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(group.groupName);
+
+  const handleSave = () => {
+    if (!name.trim()) return;
+    updateGroup(group.id, { groupName: name });
+    setIsEditing(false);
+  };
   return (
     <div className="border rounded p-4 space-y-3">
       {/* group name */}
-      <div className="font-semibold">{group.groupName}</div>
+      <div className="flex justify-between items-center">
+        {isEditing ? (
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            onBlur={handleSave}
+            className="border px-2 py-1"
+            autoFocus
+          />
+        ) : (
+          <div
+            className="font-semibold cursor-pointer"
+            onClick={() => setIsEditing(true)}
+          >
+            {group.groupName}
+          </div>
+        )}
 
-      {/* attribute placeholder */}
+        <button
+          onClick={() => deleteGroup(group.id)}
+          className="text-red-500 text-sm cursor-pointer"
+        >
+          Delete
+        </button>
+      </div>
 
-      <div className="text-sm text-gray-500">No Attributes Yet!</div>
+      {/* attributes */}
 
-      {/* Future: Add Attribute Button */}
+      <div className="space-y-2">
+        {group.attributes.length === 0 ? (
+          <div className="text-sm text-gray-500">No Attributes Yet!</div>
+        ) : (
+          group.attributes.map((attr) => (
+            <div key={attr.id} className="border p-2 text-sm">
+              {attr.name} ({attr.type})
+            </div>
+          ))
+        )}
+      </div>
 
-      <button className="text-blue-500 text-sm">+ Add Attribute</button>
+      {/* add attribute */}
+
+      <button
+        onClick={() => onAddAttribute(group.id)}
+        className="text-blue-500 text-sm"
+      >
+        + Add Attribute
+      </button>
     </div>
   );
 };
