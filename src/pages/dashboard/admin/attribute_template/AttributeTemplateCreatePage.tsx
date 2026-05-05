c5e7dbb0f6c082a0e2872c2dd5580e1867921ab9
Shahriar Hossain <shahriarh655@gmail.com>
2026-05-06 00:58:59 +0600

    feat(attribute-template): updated AttributeTemplateCreatePage with initializing some parent level states for modal

--- src/pages/dashboard/admin/attribute_template/AttributeTemplateCreatePage.tsx
+++ src/pages/dashboard/admin/attribute_template/AttributeTemplateCreatePage.tsx
@@ -1,8 +1,22 @@
 import { useAttributeTemplate } from "@/hooks/useAttributeTemplate";
+import { useState } from "react";
 import GroupSection from "./components/GroupSection";
 import TemplateHeader from "./components/TemplateHeader";
 
 const AttributeTemplateCreatePage = () => {
+  // modal related state
+  const [selectedGroupId, setSelectedGroupId] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openAttributeModal = (groupId: string) => {
+    setSelectedGroupId(groupId);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedGroupId(null);
+    setIsModalOpen(false);
+  };
   const { state } = useAttributeTemplate();
   return (
     <div className="p-6 space-y-6">
@@ -10,7 +24,7 @@ const AttributeTemplateCreatePage = () => {
       <TemplateHeader />
 
       {/* Groups */}
-      <GroupSection groups={state.groups} />
+      <GroupSection groups={state.groups} onAddAttribute={openAttributeModal} />
     </div>
   );
 };
